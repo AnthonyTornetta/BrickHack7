7d4b8777741eecd45c2416102332b03a4b750052
ehower <ethanrhower@gmail.com>
2021-02-20 15:20:54 -0500

    Added cool dolby io api

--- brickchat/src/App.js
+++ brickchat/src/App.js
@@ -1,9 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+import { reducer as voxeetReducer } from "@voxeet/react-components"
+import thunkMidleware from "redux-thunk"
+import { combineReducers, createStore, applyMiddleware } from "redux"
+import { ConferenceRoom, VoxeetProvider } from "@voxeet/react-components"
+import "@voxeet/react-components/dist/voxeet-react-components.css"
+
+const reducers = combineReducers({
+  voxeet: voxeetReducer,
+})
+
+const configureStore = () =>
+  createStore(reducers, applyMiddleware(thunkMidleware))
+
+const settings = require('secret.json');
 
 function App() {
   return (
     <div className="App">
+		<VoxeetProvider store={configureStore()}>
+			<ConferenceRoom
+			autoJoin
+			consumerKey={settings.consumerKey}
+			consumerSecret={settings.consumerSecret}
+			conferenceAlias={settings.conferenceAlias}
+			/>
+		</VoxeetProvider>
+		
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,6 +44,7 @@ function App() {
         </a>
       </header>
     </div>
+
   );
 }
 
