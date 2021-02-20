9f5b62d8c7c0f5b21e5985c0176f58837ddae9b2
ehower <ethanrhower@gmail.com>
2021-02-20 17:16:06 -0500

    Update ChatRoom.js

--- brickchat/src/ChatRoom.js
+++ brickchat/src/ChatRoom.js
@@ -39,7 +39,7 @@ class ChatRoom extends Component
       </VoxeetProvider>
       
       <Fragment>
-              <h3 style={{ textAlign: 'center' }}>Dos Paint</h3>
+              <h3 style={{ textAlign: 'center' }}>Brick Paint</h3>
               <div className="main">
                 <div className="color-guide">
                   <h5>Color Guide</h5>
