5ba448d6fad23c73cb583a4befd3981735a344ec
ehower <ethanrhower@gmail.com>
2021-02-20 17:10:39 -0500

    Added the paint code

--- brickchat/src/ChatRoom.js
+++ brickchat/src/ChatRoom.js
@@ -1,41 +1,49 @@
+import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import { Component, Fragment } from 'react';
+import Canvas from './canvas';
 
-import { reducer as voxeetReducer } from "@voxeet/react-components";
-import thunkMidleware from "redux-thunk";
-import { combineReducers, createStore, applyMiddleware } from "redux";
-import { ConferenceRoom, VoxeetProvider } from "@voxeet/react-components";
-import "@voxeet/react-components/dist/voxeet-react-components.css";
+import { reducer as voxeetReducer } from "@voxeet/react-components"
+import thunkMidleware from "redux-thunk"
+import { combineReducers, createStore, applyMiddleware } from "redux"
+import { ConferenceRoom, VoxeetProvider } from "@voxeet/react-components"
+import "@voxeet/react-components/dist/voxeet-react-components.css"
 
 const reducers = combineReducers({
   voxeet: voxeetReducer,
-});
+})
 
 const configureStore = () =>
-  createStore(reducers, applyMiddleware(thunkMidleware));
+  createStore(reducers, applyMiddleware(thunkMidleware))
 
 const settings = require('./secret.json');
 
-function ChatRoom() {
+function App() {
   return (
-    <div className="ChatRoom">
-      <VoxeetProvider store={configureStore()}>
-        <ConferenceRoom
-        autoJoin
-        displayActions={["mute", "screenshare", "video", "chat", "attendies"]}
-        liveRecordingEnabled={false}
-        consumerKey={settings.consumerKey}
-        consumerSecret={settings.consumerSecret}
-        conferenceAlias={'ROOM CONFERENCE - CHANGE BASED ON TITLE'}
-        />
-      </VoxeetProvider>
+    <div className="App">
+		<VoxeetProvider store={configureStore()}>
+			<ConferenceRoom
+			autoJoin
+			consumerKey={settings.consumerKey}
+			consumerSecret={settings.consumerSecret}
+			conferenceAlias={settings.conferenceAlias}
+			/>
+		</VoxeetProvider>
 		
-      <div className="ChatRoomMain">
-        
-      </div>
+	  <Fragment>
+            <h3 style={{ textAlign: 'center' }}>Dos Paint</h3>
+            <div className="main">
+              <div className="color-guide">
+                <h5>Color Guide</h5>
+                <div className="user user">User</div>
+                <div className="user guest">Guest</div>
+              </div>
+              <Canvas />
+            </div>
+      </Fragment>
     </div>
-
   );
 }
 
-export default ChatRoom;
+export default App;
