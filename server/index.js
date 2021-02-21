e54b9e21816cd1504ff10081a6f1baabe1b89062
CurlyQ59 <CurlyQ59@users.noreply.github.com>
2021-02-20 20:29:46 -0500

    nothing in particular

--- server/index.js
+++ server/index.js
@@ -2,22 +2,11 @@ console.log('Really cool server');
 
 class Tag
 {
-  constructor(name)
-  {
-    this.name = name;
-  }
-
-  isSimilar(s)
-  {
-    return this.name.includes(s);
-  }
+  constructor(name){this.name = name;}
+  isSimilar(s){return this.name.includes(s);}
 }
-
 
 class ChatRoom
 {
-  constructor(nameList)
-  {
-    this.nameList = nameList;
-  }
+  constructor(nameList){this.nameList = nameList;}
 }
